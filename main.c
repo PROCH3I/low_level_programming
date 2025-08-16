899641a1ad885610995057e19e8384b8e8be6510
PROCH3I <prochei165@gmail.com>
2025-08-16 02:33:31 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+  int num;
+  int num1;
+  int result;
+
+  num = 1000;
+  num1 = 5;
+  result = num * num1;
+  printf("The product of %d and %d is = %d\n" , num , num1 , result )
+  
+  return 0;
+}
